d0dda79c41630bdda57dae7a26541cce7657dd52
Rudrscode <166156016+Rudrscode@users.noreply.github.com>
2025-07-05 11:46:12 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverComponentsHmrCache:false,
-    },
+ 
     images:{
         remotePatterns :[
             {
